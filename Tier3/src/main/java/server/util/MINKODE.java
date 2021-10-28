cec0344834cf629d9add160b7abd7e72e2f8065a
Mathias <mathiashome@hotmail.com>
2021-10-28 15:29:10 +0200

    TEST

---
+++ Tier3/src/main/java/server/util/MINKODE.java
@@ -0,0 +1,13 @@
+package server.util;
+
+public enum MINKODE
+{
+  PASSWORD("Bimmer95");
+
+  public final String password;
+
+  private MINKODE(String password)
+  {
+    this.password = password;
+  }
+}
