e345e0203381d0afea5d767db89cb4a95841405a
Solaiman1991 <satar03@msn.com>
2021-11-17 01:30:07 +0100

    ok

---
+++ Tier2/Application/Model/PlayListService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AppServer.Data;
+
+namespace AppServer.Model
+{
+    public class PlayListService:IPlayListService
+
+    {
+        public Task<PlayList> CreatePlaylist(PlayList playList)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<PlayList>> GetAllPlaylist()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdatePlaylist(PlayList playlist)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletePlayList(int playListID)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
