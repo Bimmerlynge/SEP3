28342e46c06b66d5d772ac1a724140abfafc714b
Mathias <mathiashome@hotmail.com>
2021-12-03 19:05:02 +0100

    test target

---
+++ Tier3/src/main/java/server/StartServer.java
@@ -0,0 +1,34 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import server.DAO.*;
+
+import shared.Song;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@SpringBootApplication public class StartServer
+{
+  public static void main(String[] args) throws SQLException
+  {
+    SpringApplication.run(StartServer.class, args);
+    DriverManager.registerDriver(new org.postgresql.Driver());
+    ISongDAO songDAO = new SongDAO();
+    IAlbumDAO albumDAO = new AlbumDAO();
+
+    ArrayList<Song> songs = songDAO.getAllSongs();
+
+    IMP3DAO dao = new MP3DAO();
+    dao.getAllMP3();
+    System.out.println("Ud fra database");
+    for (Song song : songs)
+    {
+      System.out.println(song.toString());
+    }
+
+
+  }
+}
